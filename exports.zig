24bdd4fc56d58bcaa620a8cedb9f6d880b58a98b
Matt Knight <mattnite@protonmail.com>
2020-10-05 19:53:08 -0700

    added in perf buffer implementation

--- exports.zig
+++ exports.zig
@@ -1,2 +1,6 @@
-pub const Object = @import("src/object.zig");
+// namespaces
 pub const elf = @import("src/elf.zig");
+
+// structs
+pub const Object = @import("src/object.zig");
+pub const PerfBuffer = @import("src/perf_buffer.zig");
