7e915bc02ab2a6039ddb4d6e3912421bc5639e77
Matt Knight <mattnite@protonmail.com>
2020-10-29 22:08:23 -0700

    silenced install script

--- ci/zig-install.sh
+++ ci/zig-install.sh
@@ -11,7 +11,7 @@ LIB_DIR=/usr/local/lib
 # chech for utilities
 for CMD in jq curl sha256sum
 do
-    if ! command -v $CMD
+    if ! command -v $CMD > /dev/null
     then
         echo need to install $CMD
         exit 1
@@ -28,11 +28,11 @@ fi
 rm $TAR_DST -f
 rm $DST -rf
 
-MASTER=$(curl -q https://ziglang.org/download/index.json 2>/dev/null | jq ".[\"$1\"].\"$(uname -m)-$(uname | awk '{ print tolower($0) }')\"")
+MASTER=$(curl -sS https://ziglang.org/download/index.json | jq ".[\"$1\"].\"$(uname -m)-$(uname | awk '{ print tolower($0) }')\"")
 URL=$(echo $MASTER | jq -r .tarball)
 SHASUM=$(echo $MASTER | jq -r .shasum)
 
-curl $URL > $TAR_DST
+curl -sS $URL > $TAR_DST
 if ! echo $SHASUM /tmp/zig.tar.xz | sha256sum --check --status
 then
     echo failed checksum for zig tarball
@@ -41,7 +41,7 @@ fi
 
 rm -rf $DST
 mkdir $DST
-tar -xvf $TAR_DST -C $DST --strip-components=1
+tar -xf $TAR_DST -C $DST --strip-components=1
 
 rm $BIN_DIR/zig -f
 rm $LIB_DIR/zig -rf
