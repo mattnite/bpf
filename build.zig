5d03c1eead8f93ab126c27fb28907fdfb96bba68
Matt Knight <mattnite@protonmail.com>
2020-10-29 21:41:41 -0700

    update ci config

--- build.zig
+++ build.zig
@@ -1,8 +1,8 @@
 const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
-    var userspace_tests = b.addTest("exports.zig");
+    var tests = b.addTest("exports.zig");
 
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&userspace_tests.step);
+    test_step.dependOn(&tests.step);
 }
